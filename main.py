5e3f381dc88cc2992181face4e6b81f5bf694e47
hargetturgent-jpg <hargetturgent@gmail.com>
2025-10-04 23:26:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import requests
+import time
 
-url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
 
-response = requests.get(url)
-data = response.json()
+while True:
+    try:
+        response = requests.get(url)
+        data = response.json()
+        if "data" in data and "amount" in data["data"]:
+            print("سعر البتكوين الحالي بالدولار:", data["data"]["amount"])
+        else:
+            print("😢 السعر على الحصول عذرا")
+    except Exception as e:
+        print("حدث خطأ:", e)
 
-# بعض الردود من API ترجع قائمة، لذا نتأكد
-if isinstance(data, dict) and "price" in data:
-    print("سعر البتكوين الحالي بالدولار:", data["price"])
-else:
-    print("تعذر الحصول على السعر 😢")
-  
+    time.sleep(60)
+    
