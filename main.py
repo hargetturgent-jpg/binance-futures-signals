0ff02a9cfc268af1e5c3cc4fb0ee26adb72c5f13
hargetturgent-jpg <hargetturgent@gmail.com>
2025-10-04 23:10:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 response = requests.get(url)
 data = response.json()
 
-print("سعر البيتكوين الحالي بالدولار:", data["price"])
+print("سعر البتكوين الحالي بالدولار:", data.get("price", "غير متوفر"))
